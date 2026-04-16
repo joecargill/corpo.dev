88ca64da91816619a4c6b6ae309a34f63bebaf7e
Joe Cargill <joe.cargill@maestro-media.com>
2026-04-16 21:57:39 +0100

    tweak: logo img

--- quartz/components/LogoImage.tsx
+++ quartz/components/LogoImage.tsx
@@ -3,8 +3,8 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 export default (() => {
   const LogoImage: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8080" : `https://${cfg.baseUrl}`; 
-    const source = `${baseUrl}/static/developedbycorpo-white.png`;
-    return <img src={source}></img>
+    const source = `${baseUrl}/static/hardwarerpg-logo.png`;
+    return <img style="color: #fff;"src={source}></img>
   }
  
   return LogoImage
